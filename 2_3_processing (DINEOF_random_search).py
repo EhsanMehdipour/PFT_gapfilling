cbb3e9a91afcd0157dc8bfc64565d23b3689fb53
EhsanMehdipour <mehdipour.eh@gmail.com>
2025-02-18 22:52:29 +0100

    remove figure and data

---
+++ 2_3_processing (DINEOF_random_search).py
@@ -0,0 +1,230 @@
+'''
+**Project**
+
+Assessment of gap-filling techniques applied to satellite phytoplankton composition products for the Atlantic Ocean
+
+**Credit**
+
+**© Ehsan Mehdipour**, 2025. (ehsan.mehdipour@awi.de)
+
+Alfred Wegener Insitute for Polar and Marine Research, Bremerhaven, Germany
+
+This work is licensed under the **GNU General Public License v3.0 (GPL-3.0)**.  
+
+**Objective**
+
+This script creates a random initialization (init) file for DINEOF.
+
+**Dataset**
+
+The dataset is accessable through Copernicus Marine Service with the following DOI:
+https://doi.org/10.48670/moi-00280 and Dataset ID: cmems_obs-oc_glo_bgc-plankton_my_l3-multi-4km_P1D
+'''
+
+## Import modules
+
+import os
+import math
+import random
+import argparse
+import json
+from params import params
+
+## parse the configuration from them SLURM file
+parser = argparse.ArgumentParser(description="get the experiment number")
+parser.add_argument('--experiment', type=int, required=True, help='the experiment number from slurm array')
+parser.add_argument('--region', type=int, required=True, help='the region number from slurm code')
+args = parser.parse_args()
+experiment = args.experiment
+region = args.region
+
+# Random Creation
+sst = random.choice([True, False])
+numit = random.randrange(1, 20)
+alpha = round(0.5 * 10**(-2 * random.random()),3)
+
+rec = 1
+norm = 0
+nev = 200
+
+filter_extent = round(2 * math.pi * math.sqrt(float(alpha)*float(numit)),2)
+
+# Save json file
+json_dict = {
+    'sst':sst,
+    'numit':numit,
+    'alpha':alpha,
+    'filter_extent':filter_extent,
+    'rec':rec,
+    'norm':norm,
+    'nev':nev,
+    }
+
+with open(f'random/json/{region}_{experiment}.json', 'w') as file:
+    json.dump(json_dict, file, indent=4)  # indent=4 for pretty-printing
+
+## list of PFT parameters
+PFT = params['PFT']
+
+if sst:
+    PFT = PFT + ['sst']
+
+working_dir = os.path.join(params['output_dir'], region)
+
+## Input
+data_file = os.path.join(working_dir,'ds_pft_dev_filled.nc')
+mask_file = os.path.join(working_dir,'ds_pft_dev_filled.nc')
+cloud_index = os.path.join(working_dir,'cloud_index_train.nc')
+
+## Output
+reconstuct_dir = os.path.join(working_dir,'DINEOF',experiment)
+os.makedirs(reconstuct_dir, exist_ok=True)
+output_file = os.path.join(working_dir,'ds_reconstructed')
+EOF_dir = reconstuct_dir
+
+## Creating the path to the output files
+data = [f"'{data_file}#{pft}'" for pft in PFT]
+data_str = ','.join(data)
+
+mask = [f"'{mask_file}#mask'" for pft in PFT]
+mask_str = ','.join(mask)
+
+results = [f"'{output_file}_{pft}.nc#{pft}'" for pft in PFT]
+results_str = ','.join(results)
+
+EOF_U = [f"'{EOF_dir}/EOF_{pft}.nc#{pft}'" for pft in PFT]
+EOF_U_str =  ','.join(EOF_U)
+
+## Creating the init file for running DINEOF
+with open(f"random/{region}_{experiment}.init", "w") as f:
+    f.write(f"""
+!
+! INPUT File for dineof 2.0
+!
+! Lines starting with a ! or # are comments
+!
+
+! gappy data to fill by DINEOF. For several matrices, separate names with commas 
+! Example:  
+!          data = ['seacoos2005.avhrr','seacoos2005.chl']
+
+data = [{data_str}]
+
+
+!--------------MASK FILE----------------------------------------------
+! Land-sea mask of the gappy data. 
+! Several masks can be especified, separated by commas:
+! Example : 
+!           mask = ['seacoos2005.avhrr.mask','seacoos2005.chl.mask']
+!
+! When no mask is especified (comment out the maskfile line), 
+!                      no land points are present in the initial file
+
+mask = [{mask_str}]
+
+!---------------------------------------------------------------------
+
+time = '{data_file}#time'
+alpha = {alpha}
+numit = {numit}
+
+
+!
+! Sets the numerical variables for the computation of the required
+! singular values and associated modes.
+!
+! Give 'nev' the maximum number of modes you want to compute 
+
+nev = {nev}
+
+! Give 'neini' the minimum  number of modes you want to compute 
+
+neini = 1
+
+! Give 'ncv' the maximal size for the Krylov subspace 
+! (Do not change it as soon as ncv > nev+5) 
+! ncv must also be smaller than the temporal size of your matrix
+
+ncv = {nev+5}
+
+! Give 'tol' the treshold for Lanczos convergence 
+! By default 1.e-8 is quite reasonable 
+
+tol = 1.0e-8
+
+! Parameter 'nitemax' defining the maximum number of iteration allowed for the stabilisation of eofs obtained by the cycle ((eof decomposition <-> truncated reconstruction and replacement of missing data)). An automatic criteria is defined by the following parameter 'itstop' to go faster 
+
+nitemax = 300
+
+! Parameter 'toliter' is a precision criteria defining the threshold of automatic stopping of dineof iterations, once the ratio (rms of successive missing data reconstruction)/stdv(existing data) becomes lower than 'toliter'. 
+
+toliter = 1.0e-3
+
+! Parameter 'rec' for complete reconstruction of the matrix 
+! rec=1 will reconstruct all points; rec=0 only missing points
+
+rec = {rec}
+
+! Parameter 'eof' for writing the left and right modes of the
+!input matrix. Disabled by default. To activate, set to 1
+
+eof = 1
+
+! Parameter 'norm' to activate the normalisation of the input matrix
+!for multivariate case. Disabled by default. To activate, set to 1
+
+norm = {norm}
+
+! Output folder. Left and Right EOFs will be written here     
+!
+
+!Output = './'
+Output = '{reconstuct_dir}'
+
+!
+! user chosen cross-validation points,
+! remove or comment-out the following entry if the cross-validation points are to be chosen
+! internally
+!
+
+! clouds = 'crossvalidation.clouds'
+
+!
+! "results" contains the filenames of the filled data
+!
+
+!results = ['All_95_1of2.sst.filled']
+!results = ['Output/F2Dbelcolour_region_period_datfilled.gher']
+
+results = [{results_str}]
+
+
+! seed to initialize the random number generator
+
+seed = 243435
+
+EOF.U = [{EOF_U_str}]
+EOF.V = '{EOF_dir}/EOF_V.nc#V'
+EOF.Sigma = '{EOF_dir}/EOF_Sigma.nc#Sigma'
+
+!-------------------------!
+! cross-validation points !
+!-------------------------!
+
+!number_cv_points = 7000
+
+!cloud surface size in pixels 
+!cloud_size = 500
+
+
+
+clouds = '{cloud_index}#clouds_index'
+
+
+!
+! END OF PARAMETER FILE 
+!
+
+
+
+    """)
---
+++ 3_1_processing (DINCAE_final).jl
@@ -0,0 +1,253 @@
+"""
+**Project**
+
+Assessment of gap-filling techniques applied to satellite phytoplankton composition products for the Atlantic Ocean
+
+**Credit**
+
+Adapted from the DINCAE 2.0 example files 
+https://github.com/gher-uliege/DINCAE.jl/blob/main/examples/DINCAE_tutorial.jl
+
+**Objective**
+
+This script creates a final run for DINCAE based on saved configuration.
+
+**Dataset**
+
+The dataset is accessable through Copernicus Marine Service with the following DOI:
+https://doi.org/10.48670/moi-00280 and Dataset ID: cmems_obs-oc_glo_bgc-plankton_my_l3-multi-4km_P1D
+"""
+
+Threads.nthreads()
+
+#--------------------------------------------------------------------
+# Import modules
+
+using Pkg
+Pkg.activate(".../DINCAE/DINCAE.jl")
+# Pkg.add(PackageSpec(name="CUDA", version="5.2.0"))
+Pkg.instantiate()
+
+
+using DINCAE
+using DINCAE_utils
+using CUDA
+using Dates
+using NCDatasets
+using Random
+using PyPlot
+using JSON
+
+#--------------------------------------------------------------------
+# Parse command-line arguments
+args = collect(ARGS)
+region = parse(Int, args[1])
+
+#--------------------------------------------------------------------
+## Loading file
+
+## Local directory
+localdir = expanduser(".../regions/$region")
+
+## Create directory
+# mkpath(localdir)
+
+## Filename of the data with added clouds for cross-validation
+fname_cv = joinpath(localdir,"ds_pft_dev.nc")
+# varname = "sst"
+
+outdir = joinpath(localdir,"DINCAE/final")
+mkpath(outdir)
+
+#--------------------------------------------------------------------
+## CUDA
+
+const F = Float32
+
+if CUDA.functional()
+    Atype = CuArray{F}
+else
+    @warn "No supported GPU found. We will use the CPU which is very slow. Please check https://developer.nvidia.com/cuda-gpus"
+    Atype = Array{F}
+end
+
+#--------------------------------------------------------------------
+## Address to the optimal configuration json file
+param_file = ".../9/DINCAE/106/param.json"
+
+hyperparams = JSON.parsefile(param_file)
+
+# Accessing the hyperparameters
+sst = hyperparams["sst"]  # This will be a boolean
+uncertainty_type = hyperparams["uncertainty_type"]
+jitter_std = hyperparams["jitter_std"]
+epochs = hyperparams["epochs"]
+save_epochs = hyperparams["save_epochs"]
+batch_size = hyperparams["batch_size"]
+enc_nfilter_internal = hyperparams["enc_nfilter_internal"]
+ntime_win = hyperparams["ntime_win"]
+upsampling_method = Symbol(hyperparams["upsampling_method"])  # This will be a string
+learning_rate = hyperparams["learning_rate"]
+regularization_L2_beta = hyperparams["regularization_L2_beta"]
+loss_weights_refine = Tuple(hyperparams["loss_weights_refine"])
+laplacian_penalty = hyperparams["laplacian_penalty"]
+truth_uncertain = hyperparams["truth_uncertain"]  # This will be a boolean
+remove_mean = hyperparams["remove_mean"]  # This will be a boolean
+
+#--------------------------------------------------------------------
+# Data Parameters
+
+if uncertainty_type=="spatial"
+
+    data_param = [
+        (filename = fname_cv,
+         varname = "CHL",
+         errvarname = "CHL_uncertainty",
+         jitter_std = jitter_std,
+         isoutput = true,
+         ),
+        (filename = fname_cv,
+         varname = "DIATO",
+         errvarname = "DIATO_uncertainty",
+         jitter_std = jitter_std,
+         isoutput = true,
+         ),
+        (filename = fname_cv,
+         varname = "DINO",
+         errvarname = "DINO_uncertainty",
+         jitter_std = jitter_std,
+         isoutput = true,
+         ),
+        (filename = fname_cv,
+         varname = "HAPTO",
+         errvarname = "HAPTO_uncertainty",
+         jitter_std = jitter_std,
+         isoutput = true,
+         ),
+        (filename = fname_cv,
+         varname = "GREEN",
+         errvarname = "GREEN_uncertainty",
+         jitter_std = jitter_std,
+         isoutput = true,
+         ),
+        (filename = fname_cv,
+         varname = "PROKAR",
+         errvarname = "PROKAR_uncertainty",
+         jitter_std = jitter_std,
+          isoutput = true,
+         ),
+    ]
+
+    if sst
+        push!(data_param, (
+            filename = fname_cv,
+            varname = "sst",
+            errvarname = "sst_uncertainty",
+            jitter_std = jitter_std,
+            isoutput = false,
+        ))
+    end
+
+elseif uncertainty_type=="equal" || uncertainty_type=="median"
+    
+    if uncertainty_type=="equal"
+        
+        CHL_err_std = 1
+        DIATO_err_std = 1
+        DINO_err_std = 1
+        HAPTO_err_std = 1
+        GREEN_err_std = 1
+        PROKAR_err_std = 1
+        sst_err_std = 1
+        
+    elseif uncertainty_type=="median"
+        
+        CHL_err_std = 0.118f0
+        DIATO_err_std = 0.381079f0
+        DINO_err_std = 0.3274f0
+        HAPTO_err_std = 0.342107f0
+        GREEN_err_std = 0.254379f0
+        PROKAR_err_std = 0.299965f0
+        sst_err_std = 0.00791152f0
+        
+    end
+    
+    data_param = [
+        (filename = fname_cv,
+         varname = "CHL",
+         obs_err_std = CHL_err_std,
+         jitter_std = jitter_std,
+         isoutput = true,
+         ),
+        (filename = fname_cv,
+         varname = "DIATO",
+         obs_err_std = DIATO_err_std,
+         jitter_std = jitter_std,
+         isoutput = true,
+         ),
+        (filename = fname_cv,
+         varname = "DINO",
+         obs_err_std = DINO_err_std,
+         jitter_std = jitter_std,
+         isoutput = true,
+         ),
+        (filename = fname_cv,
+         varname = "HAPTO",
+         obs_err_std = HAPTO_err_std,
+         jitter_std = jitter_std,
+         isoutput = true,
+         ),
+        (filename = fname_cv,
+         varname = "GREEN",
+         obs_err_std = GREEN_err_std,
+         jitter_std = jitter_std,
+         isoutput = true,
+         ),
+        (filename = fname_cv,
+         varname = "PROKAR",
+         obs_err_std = PROKAR_err_std,
+         jitter_std = jitter_std,
+         isoutput = true,
+         ),
+    ]
+
+    if sst
+        push!(data_param, (
+            filename = fname_cv,
+            varname = "sst",
+            obs_err_std = sst_err_std,
+            jitter_std = jitter_std,
+            isoutput = false,
+        ))
+    end
+
+end
+
+data_test = data_param;
+fnames_rec = [joinpath(outdir,"ds_reconstructed.nc")]
+data_all = [data_param,data_test]
+varname = data_all[1][1].varname
+
+paramfile = joinpath(outdir,"paramfile.param")
+#---------------------------------------------------------------------
+# Run DINCAE
+
+loss = DINCAE.reconstruct(
+    Atype,data_all,fnames_rec;
+    epochs = epochs,
+    save_epochs = save_epochs,
+    batch_size = batch_size,
+    enc_nfilter_internal = enc_nfilter_internal,
+    # skipconnections = skipconnections,
+    ntime_win = ntime_win,
+    learning_rate = learning_rate,
+    upsampling_method = upsampling_method,
+    regularization_L2_beta = regularization_L2_beta,
+    loss_weights_refine = loss_weights_refine,
+    laplacian_penalty = laplacian_penalty,
+    truth_uncertain = truth_uncertain,
+    remove_mean = remove_mean,
+    paramfile = paramfile,
+)
+
+#--------------------------------------------------------------------
